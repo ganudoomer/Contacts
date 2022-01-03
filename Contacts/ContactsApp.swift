c474838157489fa7c40815110f4ff2f8688788f4
ganudoomer <ganuiscool@gmail.com>
2022-01-03 23:40:34 +0530

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Sree on 03/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
